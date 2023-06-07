c4a98cb1356b5ffbd6028e2a38426c1b3dd75931
Jonathan Leitschuh <jonathan.leitschuh@gmail.com>
2023-06-07 15:38:50 -0400

    Eagerly load GPG Key Config (#20)

--- src/omega_moderne_client/client/gpg_key_config.py
+++ src/omega_moderne_client/client/gpg_key_config.py
@@ -1,5 +1,6 @@
 import os
 from dataclasses import dataclass
+from typing import List
 
 
 @dataclass(frozen=True)
@@ -10,15 +11,28 @@ class GpgKeyConfig:
 
     @classmethod
     def load_from_env(cls) -> 'GpgKeyConfig':
+        env_vars = cls._load_env_variables(
+            "GPG_KEY_PASSPHRASE",
+            "GPG_KEY_PRIVATE_KEY",
+            "GPG_KEY_PUBLIC_KEY"
+        )
         return GpgKeyConfig(
-            key_passphrase=cls._load_env_variable("GPG_KEY_PASSPHRASE"),
-            key_private_key=cls._load_env_variable("GPG_KEY_PRIVATE_KEY"),
-            key_public_key=cls._load_env_variable("GPG_KEY_PUBLIC_KEY")
+            key_passphrase=env_vars[0],
+            key_private_key=env_vars[1],
+            key_public_key=env_vars[2]
         )
 
     @staticmethod
-    def _load_env_variable(env_name: str) -> str:
-        env = os.getenv(env_name)
-        if not env:
-            raise ValueError(f"{env_name} environment variable is not set")
-        return env
+    def _load_env_variables(*env_names: str) -> List[str]:
+        missing_env_var_names = []
+        present_env_vars = []
+        for env_name in env_names:
+            value = os.getenv(env_name)
+            if not value:
+                missing_env_var_names.append(env_name)
+            else:
+                present_env_vars.append(value)
+
+        if missing_env_var_names:
+            raise ValueError(f"Environment variables {missing_env_var_names} are not set")
+        return present_env_vars
